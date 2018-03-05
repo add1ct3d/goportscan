e7e41f5a58ee46fbb8268322bbb2e67110a9cc6e
Matt Weidner <matt.weidner@gmail.com>
2018-03-04 21:51:24 -0600

    initial

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+// File: goportscan.go
+// Date: 2018-03-04
+// Author: Matt Weidner <matt.weidner@gmail.com>
+// Description: Concurrent full port scanner
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var target string
+	build := 9
+	flag.StringVar(&target, "ip", "127.0.0.1", "target IP")
+	flag.Parse()
+	fmt.Println("goportscan build", build)
+	fmt.Println("Scanning", target)
+	wg.Add(65536)
+	for p := 0; p < 65536; p++ {
+		port := fmt.Sprintf("%d", p)
+		go func(port string) {
+			defer wg.Done()
+			//fmt.Println("Trying", port)
+			c, e := net.DialTimeout("tcp", target+":"+port, time.Second*3)
+			if e == nil {
+				fmt.Println(port + "/open")
+				c.Close()
+			}
+		}(port)
+		time.Sleep(2 * time.Millisecond)
+	}
+	wg.Wait()
+}
